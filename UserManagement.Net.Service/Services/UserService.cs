d51bd41f4b8274823ed34bc6771faf6536605b7c
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 09:28:48 +0300

    trying to solve an error

--- UserManagement.Net.Service/Services/UserService.cs
+++ UserManagement.Net.Service/Services/UserService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UserManagement.Net.Service.Services
+﻿namespace UserManagement.Net.Service.Services
 {
-    internal class UserService
+    public class UserService
     {
     }
 }
